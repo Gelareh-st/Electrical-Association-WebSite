30782f986ee96c4d03902416f3916d0ab71985e9
Soheil MEHRZI <75741680+SoheilMehrizi@users.noreply.github.com>
2022-07-08 11:42:49 +0430

    Delete web directory

--- web/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from web.models import Courses, Master, Members, People
-admin.site.register(People)
-admin.site.register(Master)
-admin.site.register(Courses)
-admin.site.register(Members)
